f9563ba3c179cc753f04334ceae3ae4fe0b9a468
rushikesh <dikeyrushikesh@gmail.com>
2022-03-08 01:06:20 +0530

    Added pizza bill generator

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Day4_of_code
+
+* Basic of nested conditions
+* Leap year calculator
+* Rollercoaster bill generator
+* Pizza bill generator
--- main.py
+++ main.py
@@ -32,23 +32,52 @@
 #     age = int(input("Enter your age: "))
 #
 #     if age < 12:
-#         print("Child tickets are $5")
+#         print("Child tickets are ₹5")
 #         bill = 5
 #     elif age <= 18:
-#         print("Youth tickets are $7")
+#         print("Youth tickets are ₹7")
 #         bill = 7
 #     else:
-#         print("Adult tickets are $12")
+#         print("Adult tickets are ₹12")
 #         bill = 12
 #     photo = input("Do you want photograph? enter Y or N: ")
 #
 #     if photo == 'Y':
 #         bill = bill +3
-#         print(f"Total bill is {bill}")
+#         print(f"Total bill is ₹ {bill}")
 #     else:
-#         print(f"Total bill is {bill}")
+#         print(f"Total bill is ₹ {bill}")
 #
 # else:
 #     print("Sorry, you can grow your taller before you ride")
 
+# Pizza bill generator
 
+print("Welcome to DK's Pizza")
+
+size = input("What size pizza do you want S, M or L: ")
+pepperoni = input("Do you want pepperoni Y or N: ")
+chesse = input("Do you want extra chesse Y or N: ")
+
+bill = 0
+
+if size == 'S':
+    bill = 250
+elif size == 'M':
+    bill  = 550
+else:
+    bill = 850
+
+if pepperoni == 'Y':
+    if size == 'S':
+        bill += 50
+    elif size == 'M':
+        bill += 80
+    elif size == 'L':
+        bill +=110
+if chesse == 'Y':
+        bill += 30
+else:
+    print("Try adding cheese")
+
+print(f"Your final bill is: ${bill}.")
