7fbd0f3864889b84663e2f82a7b111497301b511
rushikesh <dikeyrushikesh@gmail.com>
2022-03-08 00:09:33 +0530

    Added Leap year calculator and rollercoster bill generator

--- main.py
+++ main.py
@@ -4,3 +4,51 @@
 
 # Author: Rushikesh Dikey
 # Date: 07-03-2022
+
+# Leap year calculator
+
+# year = int(input("Enter year to check is it a leap year or not "))
+#
+# if(year%4 == 0):
+#     if(year%100 == 0):
+#         if(year%400 == 0):
+#             print("Leap year")
+#         else:
+#             print("Not a leap year")
+#     else:
+#         print("Leap year")
+# else:
+#     print("Not a leap year")
+#
+# Rollercoaster entry
+
+# print("Welcome to the Rollercoaster\n")
+#
+# hieght = int(input("What is your hieght in cms? "))
+# bill = 0
+#
+# if hieght >= 120:
+#     print("You can ride the rollercoster")
+#     age = int(input("Enter your age: "))
+#
+#     if age < 12:
+#         print("Child tickets are $5")
+#         bill = 5
+#     elif age <= 18:
+#         print("Youth tickets are $7")
+#         bill = 7
+#     else:
+#         print("Adult tickets are $12")
+#         bill = 12
+#     photo = input("Do you want photograph? enter Y or N: ")
+#
+#     if photo == 'Y':
+#         bill = bill +3
+#         print(f"Total bill is {bill}")
+#     else:
+#         print(f"Total bill is {bill}")
+#
+# else:
+#     print("Sorry, you can grow your taller before you ride")
+
+
